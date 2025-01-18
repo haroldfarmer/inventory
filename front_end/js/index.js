9f9e2369af1f51626f9fba6b33deab7df5b7e1be
Harold Farmer <haroldfarmer30@gmail.com>
2025-01-18 16:34:27 -0500

    Call the api that was created

---
+++ front_end/js/index.js
@@ -0,0 +1,41 @@
+async function upLoad() {
+  const product_name = document.getElementById("product_name").value;
+  const quantity_available =
+    document.getElementById("quantity_available").value;
+  const cost = document.getElementById("cost").value;
+
+  const response = await fetch("http://localhost:3000/insert", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ product_name, quantity_available, cost }),
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+  alert(
+    `Uploaded: ${product_name} Quantity: ${quantity_available} Cost: ${cost}`
+  );
+}
+
+async function getProduct() {
+  const product_name = "Name";
+
+  try {
+    const response = await fetch(`http://localhost:3000/data/${product_name}`);
+
+    const data = await response.json();
+    const data_dict = data[0];
+    const quantity = data_dict.quantity_available;
+    document.getElementById("product_name").value = product_name;
+    document.getElementById("quantity_available").value = quantity;
+    console.log(data);
+    console.log(data[0].quantity_available);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+window.onload = getProduct;
