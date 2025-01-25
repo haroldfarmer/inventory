17f995186b904dc4ba18485becf721bc05e825ca
Harold Farmer <haroldfarmer30@gmail.com>
2025-01-25 17:10:02 -0500

    Created a update funtion that calls the update API

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -1,27 +1,67 @@
+async function update() {
+  const product_name = document.getElementById("product_name").value;
+  const quantity_available =
+    document.getElementById("quantity_available").value;
+  const cost = document.getElementById("cost").value;
+  try {
+    const response = await fetch("http://localhost:3000/update", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product_name, quantity_available, cost }),
+    });
+
+    const status = response.status;
+
+    const data = await response.json();
+
+    if (status === 200 || status === 201) {
+      alert(
+        `Updated: ${product_name} Quantity: ${quantity_available} Cost: ${cost}`
+      );
+    } else {
+      alert(`Failed to updated. ${data.message || "Something went wrong."}`);
+    }
+  } catch (error) {
+    console.error("Error during updated:", error);
+    alert(`Failed to updated data: ${error.message}`);
+  }
+}
+
 async function upLoad() {
   const product_name = document.getElementById("product_name").value;
   const quantity_available =
     document.getElementById("quantity_available").value;
   const cost = document.getElementById("cost").value;
+  try {
+    const response = await fetch("http://localhost:3000/insert", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product_name, quantity_available, cost }),
+    });
 
-  const response = await fetch("http://localhost:3000/insert", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ product_name, quantity_available, cost }),
-  });
+    const status = response.status;
 
-  const data = await response.json();
+    const data = await response.json();
 
-  console.log(data);
-  alert(
-    `Uploaded: ${product_name} Quantity: ${quantity_available} Cost: ${cost}`
-  );
+    if (status === 200 || status === 201) {
+      alert(
+        `Uploaded: ${product_name} Quantity: ${quantity_available} Cost: ${cost}`
+      );
+    } else {
+      alert(`Failed to upload. ${data.message || "Something went wrong."}`);
+    }
+  } catch (error) {
+    console.error("Error during upload:", data);
+    alert(`Failed to upload data: ${error.message}`);
+  }
 }
 
 async function getProduct() {
-  const product_name = "Name";
+  const product_name = "Roids";
 
   try {
     const response = await fetch(`http://localhost:3000/data/${product_name}`);
@@ -31,8 +71,6 @@ async function getProduct() {
     const quantity = data_dict.quantity_available;
     document.getElementById("product_name").value = product_name;
     document.getElementById("quantity_available").value = quantity;
-    console.log(data);
-    console.log(data[0].quantity_available);
   } catch (error) {
     console.log(error);
   }
