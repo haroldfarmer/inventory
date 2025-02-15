a215e17176d9cb74cc5a4d96fbd7945cf142280d
Harold Farmer <haroldfarmer30@gmail.com>
2025-02-15 17:02:59 -0500

    Create the user table to store hashed passwords

--- db/db.sql
+++ db/db.sql
@@ -4,3 +4,9 @@ CREATE TABLE products (
     cost DECIMAL(10, 2) NOT NULL,
     product_name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    hashed_password TEXT NOT NULL
+);
