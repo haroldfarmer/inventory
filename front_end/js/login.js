c5a1f896f09ed19fb3fdf2a3d2048c786e11e39a
Harold Farmer <haroldfarmer30@gmail.com>
2025-02-20 07:23:20 -0500

    Calls the login function

--- front_end/js/login.js
+++ front_end/js/login.js
@@ -1,35 +1,37 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-bcrypt.genSalt(saltRounds, (err, salt) => {
+function getSaltWrounds() {
+  bcrypt.genSalt(saltRounds, (err, salt) => {
     if (err) {
-        return;
+      return;
     }
+  });
+}
 
-
-});
-
-const userPassword = 'user_password';
-bcrypt.hash(userPassword, salt, (err, hash) => {
+function getHashedPassword(userPassword) {
+  bcrypt.hash(userPassword, salt, (err, hash) => {
     if (err) {
-        return;
+      return;
     }
 
     console.log("Hashed Password:", hash);
-});
+  });
+}
 
+const storedHashedPassword = "hashed_password_from_database";
+const userInputPassword = "password_attempt_from_user";
 
-const storedHashedPassword = 'hashed_password_from_database';
-const userInputPassword = 'password_attempt_from_user';
-
-bcrypt.compare(userInputPassword, storedHashedPassword, (err, result) => {
+function comparePasswords(userInputPassword, storedHashedPassword) {
+  bcrypt.compare(userInputPassword, storedHashedPassword, (err, result) => {
     if (err) {
-        console.error("Error compairing passwords:", err);
+      console.error("Error compairing passwords:", err);
     }
 
     if (result) {
-        console.log('Passwords match! User authenticated');
+      console.log("Passwords match! User authenticated");
     } else {
-        console.log('Passwords dont match')
+      console.log("Passwords dont match");
     }
-});
+  });
+}
